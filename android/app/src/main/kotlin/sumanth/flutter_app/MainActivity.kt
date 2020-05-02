583c936a0ad834ad6e2912b48359696c62fc6fec
Sumanth Hegde <sumanthhegde1029@gmail.com>
2020-05-02 15:40:51 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/sumanth/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package sumanth.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: Text("yo mama"),
+      backgroundColor: Colors.red[900],
+    ),
+    backgroundColor: Colors.amber,
+    body: Center(child: Text("to to ot o o")),
+  )));
+}
